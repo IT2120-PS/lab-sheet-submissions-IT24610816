a7141fcc6f3291b43df6057854efb6316601f292
IT24610816 <IT24610816@my.sliit.lk>
2025-09-16 17:58:43 +0530

    IT24610816_Lab_07

---
+++ Lab Sheet 07/IT24610816.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\User\\OneDrive\\Desktop\\IT24610816")
+getwd()
+
+#1. 
+punif(25,min = 0,max = 40) - punif(10,min = 0,max = 40)
+
+#2
+pexp(2,rate = 1/3)
+
+#3
+#i.
+1-pnorm(130,mean = 100,sd = 15)
+
+#ii
+qnorm(0.95,mean = 100,sd=15)
